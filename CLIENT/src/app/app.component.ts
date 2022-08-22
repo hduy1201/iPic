8ad5b9e83e9c69a20c43252073a426f85d82b7d7
An Teemo <an.hnt12250@sinhvien.hoasen.edu.vn>
2022-08-22 15:44:33 +0700

    UI Khung tim kiem

--- CLIENT/src/app/app.component.ts
+++ CLIENT/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { idToken } from '@angular/fire/auth';
 import { Store } from '@ngrx/store';
 import { AuthState } from 'src/states/auth.state';
@@ -9,17 +9,43 @@ import * as AuthActions from '../actions/auth.action';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   items = [{ title: 'Tạo Ghim ý tưởng' }, { title: 'Tạo Ghim' }];
-
+  recentSearchs = ['Cat', 'Dog', 'Su Tu Ha Dong'];
   title = 'CLIENT';
+
+  suggestBox: any;
+  input: any;
 
   idToken$ = this.store.select((state) => state.auth.idToken);
 
-  constructor(private store: Store<{ auth: AuthState }>) { }
+  constructor(
+    private store: Store<{ auth: AuthState }>,
+    private element: ElementRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.suggestBox = this.element.nativeElement.querySelector(
+      '.suggestionContainer'
+    );
+
+    this.input = this.element.nativeElement.querySelector('#searchInput');
+  }
 
   login() {
     this.store.dispatch(AuthActions.login());
   }
 
+  onFocus() {
+    this.suggestBox.classList.add('make-visible');
+  }
+
+  onFocusout() {
+    if (
+      this.input === document.activeElement ||
+      this.suggestBox.matches(':hover')
+    )
+      return;
+    this.suggestBox.classList.remove('make-visible');
+  }
 }
