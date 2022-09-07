a3113fc795fb4877ee124fa0a93609b1eca7f514
Trong Pham <trong.phamtranduc@gmail.com>
2022-09-07 11:07:50 +0700

    fix

--- CLIENT/src/app/services/suggest-keyword.service.ts
+++ CLIENT/src/app/services/suggest-keyword.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { URL } from 'src/configs/baseURL';
 import { Post } from 'src/models/post';
 
 @Injectable({
@@ -12,7 +13,7 @@ export class SuggestKeywordService {
   constructor(private http: HttpClient) { }
 
   getSuggestKeyword(keyword: string): Observable<Post[]> {
-    let suggests = this.http.get<Post[]>(`http://localhost:3000/post/suggest?title=${keyword}&page=1&pagesize=10`)
+    let suggests = this.http.get<Post[]>(URL + `post/suggest?title=${keyword}&page=1&pagesize=10`)
     return suggests;
   }
 }
